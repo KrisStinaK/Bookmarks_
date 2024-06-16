bba63cc340d64615cd1328726abdd19f46d9b75c
KrisStinaK <112583110+KrisStinaK@users.noreply.github.com>
2024-06-17 01:36:42 +0300

    Регистрация пользователя

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -4,7 +4,30 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Создать новый объект пользователя,
+            # но пока не сохранять его
+            new_user = user_form.save(commit=False)
+            # Установить выбранный пароль
+            new_user.set_password(
+                user_form.cleaned_data['password'])
+            # Сохранить объект User
+            new_user.save()
+            return render(request,
+                          'account/template/register_done.html',
+                          {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+
+    return render(request,
+                  'account/template/register.html',
+                  {'user_form': user_form})
 
 
 def user_login(request):
@@ -33,3 +56,4 @@ def user_login(request):
 def dashboard(request):
     return render(request, 'account/dashboard.html',
                   {'selection': 'dashboard'})
+
