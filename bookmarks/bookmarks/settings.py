7e28ab6238273d4db0132a29ac9cbc33bf9c42a2
KrisStinaK <112583110+KrisStinaK@users.noreply.github.com>
2024-08-20 20:45:21 +0300

    Added js files for bookmarklet

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -19,6 +19,15 @@ DEBUG = True
 ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1']
 
 
+if DEBUG:
+    import mimetypes
+    mimetypes.add_type('application/javascript', '.js', True)
+    mimetypes.add_type('text/css', '.css', True)
+
+
+
+
+
 # Application definition
 
 INSTALLED_APPS = [
